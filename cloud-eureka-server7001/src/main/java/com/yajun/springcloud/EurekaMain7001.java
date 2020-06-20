2726d1451b16a650a2f1080889f46a57832bd0b6
亚军 <zyjtoto@163.com>
2020-06-20 22:02:21 +0800

    配置支付模块注册到eurekaServer

--- cloud-eureka-server7001/src/main/java/com/yajun/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/yajun/springcloud/EurekaMain7001.java
@@ -2,19 +2,20 @@ package com.yajun.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 /**
  *
- * 功能描述: 服务提供端 提供创建订单，查询订单功能
+ * 功能描述: 服务治理，服务注册中心
  *
  * @param:
  * @return:
  * @auther: zyj
- * @date: 2020/6/14 16:10
+ * @date: 2020/6/20 19:47
  */
 @SpringBootApplication
-
-public class PaymentMain {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain.class, args);
+        SpringApplication.run(EurekaMain7001.class, args);
     }
 }
